abbb3a4066a68bc10f48e112da36975681a7c19d
Timothy Meixell <misternu@gmail.com>
2015-10-21 11:56:05 -0700

    Add reflection

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -5,6 +5,36 @@ middle = gets.chomp
 puts "What is your last name?"
 last = gets.chomp
 puts "Hello, " + first + " " + middle + " " + last + "!"
+
+
 puts "What is your favorite number?"
 number = gets.chomp.to_i
 puts "Maybe " + (number + 1).to_s + " is better?"
+
+
+## How do you define a local variable?
+
+# type the name of the variable, an equals sign, then the value of the variable
+
+## How do you define a method?
+
+# type def, the name of the method, the parameters. When the method is over, type end
+
+## What is the difference between a local variable and a method?
+
+# a local variable stores an object, a method stores commands
+
+## How do you run a ruby program from the command line?
+
+# type ruby followed by any options and the file name
+
+## How do you run an RSpec file from the command line?
+
+# type rspec followed by any options and the file name
+
+## What was confusing about this material? What made sense?
+
+# the whole thing made sense, though I wasn't sure why we wrote math methods that just called other methods
+
+### For the code that I completed, click 'week-4' above (I'm assuming you're in github)
+### The folders associated with this challenge are address, define-method, math
