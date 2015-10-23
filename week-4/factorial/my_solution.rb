118a6a7c7add3444c42604e5e77c9050c21f4007
Timothy Meixell <misternu@gmail.com>
2015-10-22 19:32:47 -0700

    Add factorial

---
+++ week-4/factorial/my_solution.rb
@@ -0,0 +1,26 @@
+# Your Solution Below
+def factorial(number)
+  result = 1
+  i = 0
+  while i < number
+  i = i+1
+  result = result * i
+  end
+  return result
+end
+
+
+### Pseudocode ###
+
+=begin
+define method
+ factorial result to one
+ counter tracks number of products (set to zero)
+ begin while loop
+  while counter is less than initial number
+  counter increases by one
+  factorial = factorial times counter
+  loop is repeated until number is reached with counter
+  end
+  return factorial
+=end
