0ceb15c8eeb281e75361477eb0c72c7f377873c4
Timothy Meixell <misternu@gmail.com>
2015-11-06 17:22:54 -0600

    6.5 solution and other work

---
+++ week-6/nested_data_solution.rb
@@ -0,0 +1,92 @@
+# RELEASE 2: NESTED STRUCTURE GOLF
+# Hole 1
+# Target element: "FORE"
+
+array = [[1,2], ["inner", ["eagle", "par", ["FORE", "hook"]]]]
+
+# attempts:
+# ============================================================
+# p array[1][1][2][0]
+
+
+# ============================================================
+
+# Hole 2
+# Target element: "congrats!"
+
+hash = {outer: {inner: {"almost" => {3 => "congrats!"}}}}
+
+# attempts:
+# ============================================================
+# p hash[:outer][:inner]["almost"][3]
+
+
+# ============================================================
+
+
+# Hole 3
+# Target element: "finished"
+
+nested_data = {array: ["array", {hash: "finished"}]}
+
+# attempts:
+# ============================================================
+# p nested_data[:array][1][:hash]
+
+
+# ============================================================
+
+# RELEASE 3: ITERATE OVER NESTED STRUCTURES
+
+number_array = [5, [10, 15], [20,25,30], 35]
+
+# number_array.map! do |x|
+#   if x.class == Array
+#     x.to_a.map! do |y|
+#       y + 5
+#     end
+#   else
+#     x + 5
+#   end
+# end
+
+# p number_array.to_s
+
+
+# Refactored Solution
+
+number_array.map! do |x|
+  if x.is_a?(Array)
+    x.map do |y|
+      y + 5
+    end
+  else
+    x + 5
+  end
+end
+
+p number_array.to_s
+
+
+# Bonus:
+
+startup_names = ["bit", ["find", "fast", ["optimize", "scope"]]]
+
+
+startup_names.map! do |x|
+  if x.is_a?(Array)
+    x.map do |y|
+      if y.is_a?(Array)
+        y.map do |z|
+          z + "ly"
+        end
+      else
+        y + "ly"
+      end
+    end
+  else
+    x + "ly"
+  end
+end
+
+p startup_names
