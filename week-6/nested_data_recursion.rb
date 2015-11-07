d39b27ea4e9b290ed1609400db9f659d3a894c5f
Timothy Meixell <misternu@gmail.com>
2015-11-07 12:51:37 -0600

    recursive nested data refactor

--- week-6/nested_data_recursion.rb
+++ week-6/nested_data_recursion.rb
@@ -1,15 +1,23 @@
-class Array
-  def map_int(&block)
-    self.map do |x|
-      if x.is_a?(Integer)
-        yield x
-      elsif x.is_a?(Array)
-        x.map_int(&block)
-      else
-        x
-      end
-    end
+# class Array
+#   def map_only_class(type, &block)
+#     self.map do |x|
+#       if x.is_a?(type)
+#         yield x
+#       elsif x.is_a?(Array)
+#         x.map_only_class(type, &block)
+#       else
+#         x
+#       end
+#     end
+#   end
+# end
+
+module Enumerable
+  def map_nest (&block)
+    raise NoMethodError unless self.is_a?(Hash) || self.is_a?(Array)
+    self.map {|child| (child.is_a?(Enumerable)) ? child.map_nest(&block) : yield(child)}
   end
 end
 
-p [1,2,[1,3,[1,4,[1,5,[1,6,[1,7,[1,42]]]]]], -200].map_int {|x| x+1}
+p [1,2,[1,3,[1,4,[1,5,[1,6,[1,7,[1,42]]]]]], -200].map_nest {|x| x+1}
+p ["string", "b", ["c", "d", ["e", "f"]], "g"].map_nest {|s| s+"s"}
